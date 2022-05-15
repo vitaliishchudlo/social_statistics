3e38a7686857dd494e4f14fc7f363dcce5d4d8e5
Vitalii <vitalik.shchudlo01@ukr.net>
2022-05-15 22:54:49 +0300

    First structure of the API

--- micro-system/boot.py
+++ micro-system/boot.py
@@ -1,12 +1,25 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 # import esp
 # esp.osdebug(None)
-# mport webrepl
+# import webrepl
 # webrepl.start()
 
-from manage_network import connect_wireless_network
-print('\n\n')
+from machine import Pin
 
+from manage_network import connect_wireless_network
+
+
+def clearConsole(): return print('\n' * 25)
+
+
+clearConsole()
+
+print('= ' * 20 + '\n')
+
+network_error_pin = Pin(15, Pin.OUT)
+lcd_error_pin = Pin(0, Pin.OUT)
+lcd_error_pin.off()
+network_error_pin.off()
 
 sta_if = None
 
---
+++ micro-system/main.py
@@ -0,0 +1,16 @@
+# import time
+#
+# time.sleep(5)
+#
+# network_error_pin.on()
+#
+# time.sleep(2)
+#
+# network_error_pin.off()
+#
+# time.sleep(1)
+#
+# lcd_error_pin = Pin(0, Pin.OUT)
+#
+# lcd_error_pin.on()
+# network_error_pin.on()
